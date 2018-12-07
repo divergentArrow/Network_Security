4edf1a114915faf290791a77f717d28dd0b12ce0
Fareen Pourmoussavian <fpourmoussavian@gmail.com>
2018-12-06 23:42:01 -0500

    pushing

---
+++ RSA.py
@@ -0,0 +1,40 @@
+
+from Crypto.PublicKey import RSA
+from Crypto import Random
+import base64
+
+
+def generate_keys():
+	# RSA modulus length must be a multiple of 256 and >= 1024
+	modulus_length = 256*4 # use larger value in production
+	privatekey = RSA.generate(modulus_length, Random.new().read)
+	publickey = privatekey.publickey()
+	return privatekey, publickey
+
+
+
+def encrypt_message(a_message , publickey):
+	encrypted_msg = publickey.encrypt(a_message, 32)[0]
+	encoded_encrypted_msg = base64.b64encode(encrypted_msg)
+# base64 encoded strings are database friendly
+	return encoded_encrypted_msg
+
+
+
+def decrypt_message(encoded_encrypted_msg, privatekey):
+	decoded_encrypted_msg = base64.b64decode(encoded_encrypted_msg)
+	decoded_decrypted_msg = privatekey.decrypt(decoded_encrypted_msg)
+	return decoded_decrypted_msg
+
+a_message = "Hi friend this is being encrypted!"
+privatekey , publickey = generate_keys()
+encrypted_msg = encrypt_message(a_message , publickey)
+decrypted_msg = decrypt_message(encrypted_msg, privatekey)
+
+
+
+print("%s" % (privatekey.exportKey()))
+print("%s" % (publickey.exportKey()))
+print("Original content: %s" % (a_message))
+print("Encrypted message: %s" % (encrypted_msg))
+print("Decrypted message: %s" % (decrypted_msg))
